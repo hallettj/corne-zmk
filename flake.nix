be237aaac4022a79719154e6f2c145d6fe30bf89
Jesse Hallett <jesse@sitr.us>
2023-12-29 13:06:53 -0800

    add devShell flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+            gnumake
+          ];
+        };
+    });
+}
