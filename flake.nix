c538c96565ad81c0b54635ee04ed8e9e5a993edc
Jesse Hallett <jesse@sitr.us>
2024-05-17 11:54:39 -0700

    build with nix

--- flake.nix
+++ flake.nix
@@ -1,19 +1,65 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zmk-nix = {
+      url = "github:lilyinstarlight/zmk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, zmk-nix }:
     let
-      pkgs = nixpkgs.legacyPackages.${system};
+      eachSystem = callback: nixpkgs.lib.genAttrs systems (system: callback (zmk-pkgs system) (pkgs system));
+      systems = nixpkgs.lib.attrNames zmk-nix.packages;
+      pkgs = system: nixpkgs.legacyPackages.${system};
+      zmk-pkgs = system: zmk-nix.legacyPackages.${system} // zmk-nix.packages.${system};
+
+      zephyrDepsHash = "sha256-E/IoN2l7MLdolkXHvfq28oC9Twf6PODbl079yykk7UQ=";
+      src = nixpkgs.lib.sourceFilesBySuffices self [ ".conf" ".keymap" ".yml" ];
     in
     {
-      devShells.default = pkgs.mkShell
-        {
-          nativeBuildInputs = with pkgs; [
-            gnumake
-          ];
+      packages = eachSystem (zmk-pkgs: pkgs: rec {
+        default = firmware;
+
+        firmware = (zmk-pkgs.buildSplitKeyboard {
+          name = "corne-firmware";
+          inherit src zephyrDepsHash;
+
+          board = "nice_nano_v2";
+          shield = "corne_%PART%";
+
+          nativeBuildInputs = with pkgs; [ dtc ];
+
+          meta = {
+            description = "ZMK firmware";
+            license = nixpkgs.lib.licenses.mit;
+            platforms = nixpkgs.lib.platforms.all;
+          };
+        });
+
+        settings-reset = zmk-pkgs.buildKeyboard {
+          name = "settings-reset";
+          inherit src zephyrDepsHash;
+
+          board = "nice_nano_v2";
+          shield = "settings_reset";
+
+          nativeBuildInputs = with pkgs; [ dtc ];
+
+          meta = {
+            description = "ZMK firmware settings reset";
+            license = nixpkgs.lib.licenses.mit;
+            platforms = nixpkgs.lib.platforms.all;
+          };
         };
-    });
+
+        flash = zmk-pkgs.flash.override { inherit firmware; };
+        update = zmk-pkgs.update;
+      });
+
+      devShells = eachSystem (zmk-pkgs: pkgs: {
+        default = zmk-nix.devShells.${pkgs.system}.default;
+      });
+    };
 }
